03a36c29fe713083edc4fe31c5d6d0ded10db638
Arpita-oss <arpitarajput547@gmail.com>
2025-02-19 02:40:33 +0530

    some fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: 'https://drive-manager-frontend.vercel.app',
+  origin: 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
